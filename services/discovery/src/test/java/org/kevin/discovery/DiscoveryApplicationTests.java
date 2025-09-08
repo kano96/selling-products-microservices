4bd3bd0df70e3d32edf869c4cd2f29c5cdba7243
Kevin Torres <kevin_akd@hotmail.com>
2025-09-07 22:28:34 -0500

    feat: add config and dicovery services

---
+++ services/discovery/src/test/java/org/kevin/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kevin.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
